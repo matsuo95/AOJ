286c8ea4ca58a1c7f47569e98c813d1e481b980b
matsuo <matsuo.tomoki@pilot-inc.co.jp>
2020-03-31 15:01:46 +0900

    X Cubic

--- ITP1_1_C.cpp
+++ ITP1_1_C.cpp
@@ -1,9 +1,10 @@
 #include <iostream>
+using namespace std;
 
 int main() {
 	int a, b;
-	std::cin >> a >> b;
-	std::cout << a * b << " " << a * 2 + b * 2 << std::endl;
+	cin >> a >> b;
+	cout << a * b << " " << a * 2 + b * 2 << endl;
 
 	return 0;
 }
